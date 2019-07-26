31f54d8ad6cbc9cd1f67909e71fefbaaf720a0ab
AlexandrShipovsky <shipovsky.alexandr@yandex.ru>
2019-07-26 11:28:32 +0300

    Inital commit

---
+++ main.c
@@ -0,0 +1,93 @@
+/**
+  ******************************************************************************
+  * @file    main.c 
+  * @author  
+  * @version V
+  * @date    00-April-2019
+  * @brief   Main program body
+  ******************************************************************************
+  * @attention
+  *
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f10x.h"
+#include <stdio.h>
+
+/** @addtogroup STM32F10x_StdPeriph_Examples
+  * @{
+  */
+
+/** @addtogroup USART_Printf
+  * @{
+  */
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+/* Private function prototypes -----------------------------------------------*/
+void Delay_ms(uint32_t ms);
+/* Private functions ---------------------------------------------------------*/
+void Delay_ms(uint32_t ms)
+{
+  for (uint32_t i = 0; i < ms; i++)
+  {
+  }
+}
+
+/**
+  * @brief  Main program
+  * @param  None
+  * @retval None
+  */
+int main(void)
+{
+  /*!< At this stage the microcontroller setting
+     */
+
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+  GPIO_InitTypeDef PIN_INIT;
+  PIN_INIT.GPIO_Pin = GPIO_Pin_13;
+  PIN_INIT.GPIO_Speed = GPIO_Speed_10MHz;
+  PIN_INIT.GPIO_Mode = GPIO_Mode_Out_PP;
+  GPIO_Init(GPIOC, &PIN_INIT);
+
+  while (1)
+  {
+    GPIO_SetBits(GPIOC, GPIO_Pin_13);
+    Delay_ms(1000000);
+    GPIO_ResetBits(GPIOC, GPIO_Pin_13);
+    Delay_ms(1000000);
+  }
+}
+
+#ifdef USE_FULL_ASSERT
+
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+
+#endif
+
+/**
+  * @}
+  */
+
+/******************* (C) COPYRIGHT 2019 Biruch *****END OF FILE****/
